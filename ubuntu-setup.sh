caa73337fedfb5836aebe38bfed0bd67655b68f6
Junel Corales <jscorales@hotmail.com>
2021-08-25 14:39:50 -0400

    Add Daemon Config for Docker

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,53 +1,57 @@
 #!/bin/bash
 
-RED='\033[0;31m';
-NC='\033[0m'; # No Color
-GREEN='\033[0;32m';
-YELLOW='\033[1;33m';
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 
-CWD=`pwd`;
+CWD=$(pwd)
 
-delay_after_message=3;
+delay_after_message=3
 
 if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run with sudo (sudo -i)" 
-   exit 1
+    echo "This script must be run with sudo (sudo -i)"
+    exit 1
 fi
 
-read -p "Please enter your username: " target_user;
+read -p "Please enter your username: " target_user
 
 if id -u "$target_user" >/dev/null 2>&1; then
-    echo "User $target_user exists! Proceeding.. ";
+    echo "User $target_user exists! Proceeding.. "
 else
-    echo 'The username you entered does not seem to exist.';
-    exit 1;
+    echo 'The username you entered does not seem to exist.'
+    exit 1
 fi
 
 # function to run command as non-root user
 run_as_user() {
-	sudo -u $target_user bash -c "$1";
+    sudo -u $target_user bash -c "$1"
 }
 
-apt update;
+apt update
 apt install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common -y;
+    software-properties-common -y
 
 # Docker
-printf "${YELLOW}Installing Docker ${NC}\n";
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
-apt update;
-apt install docker-ce docker-ce-cli containerd.io -y;
+printf "${YELLOW}Installing Docker ${NC}\n"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt install docker-ce docker-ce-cli containerd.io -y
 
 # Docker Compose
-printf "${YELLOW}Installing Docker ${NC}\n";
+printf "${YELLOW}Installing Docker ${NC}\n"
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
-    -o /usr/local/bin/docker-compose;
-chmod +x /usr/local/bin/docker-compose;
+    -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-usermod -aG docker $USER;
-newgrp docker;
+# Docker user group
+usermod -aG docker $USER
+newgrp docker
+
+# Copy docker daemon config
+cp ./docker/daemon.json /etc/docker/daemon.json
